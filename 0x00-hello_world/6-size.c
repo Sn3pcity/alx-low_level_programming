eddbe4d258381fc3246fa3e3ca2d464c57675874
Sn3pcity <sn3pcity@gmail.com>
2022-10-14 19:02:55 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -19,7 +18,8 @@ int main(void)
   printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
   printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
   return (0);
-}#include <stdio.h>
+}
+#include <stdio.h>
 
  /**
   * main - Entry point
@@ -40,7 +40,8 @@ int main(void)
   printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
   printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
   return (0);
-}#include <stdio.h>
+}
+#include <stdio.h>
 
  /**
   * main - Entry point
