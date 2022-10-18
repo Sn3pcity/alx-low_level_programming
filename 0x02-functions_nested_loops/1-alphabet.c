3842fe85d873000dc91bcd520ad900a9a2ae8a4b
Sn3pcity <sn3pcity@gmail.com>
2022-10-18 04:04:03 -0700

    Alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <main.h>
+/**
+ * print_alphabet - a function that prints the alphabet, in lowercase
+ *
+ * Return: Always 0 (Succes)
+ */
+void print_alphabet(void)
+{
+int s;
+
+for (s = 'a'; s <= 'z'; s++)
+{
+putchar(s);
+}
+_putchar('\n');
+}
