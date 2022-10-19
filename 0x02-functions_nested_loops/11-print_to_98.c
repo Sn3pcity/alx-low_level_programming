3ef4afbe13b8a1f50e20fe59e97addb34b9a4471
Sn3pcity <sn3pcity@gmail.com>
2022-10-18 19:27:14 -0700

    Natural nos from N-98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,15 +9,15 @@ void print_to_98(int n)
 {
 if (n >= 98)
 {
-while (n > 98)
-putchar("%d, ", n--);
-putchar("%d\n", n);
+  while (n >98)
+scanf("%d, ", n--);
+scanf("%d\n", n);
 }
 
 else
 {
 while (n < 98)
-putchar("%d, ", n++);
-putchar("%d\n", n);
+scanf("%d, ", n++);
+scanf("%d\n", n);
 }
 }
