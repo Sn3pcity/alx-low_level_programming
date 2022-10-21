6c01c6b32e693d66b0c134cd10392aefd07980bd
Snap <113688601+Sn3pcity@users.noreply.github.com>
2022-10-21 21:22:09 +0300

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -7,8 +7,10 @@
  * Desc: Header file contains declarations for all functions
  *       used in the 0x03-debugging directory.
  */
-  void positive_or_negative(int i);
+ 
+void positive_or_negative(int i);
 int largest_number(int, int, int);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
+
 #endif
