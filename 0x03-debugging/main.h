1f9e8580e7a15df42507d5bf4171b3b445bcbaf8
Snap <113688601+Sn3pcity@users.noreply.github.com>
2022-10-21 21:23:31 +0300

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -8,7 +8,7 @@
  *       used in the 0x03-debugging directory.
  */
  
-void positive_or_negative(int i);
+void positive_or_negative(int n);
 int largest_number(int, int, int);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
