91c78fe85037701db6e63bb5993cb6e9791a0533
Sn3pcity <sn3pcity@gmail.com>
2022-10-22 05:07:18 -0700

    Uppercase Check

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,20 @@
 #include "main.h"
 int_isupper(int c);
 {
-char c;
-c = 'A';
-printf("%c : %d\n", c, _isupper(c));
-c = 'a';
-printf("%c : %d\n", c, _isupper(c));
-return (0);
+char uppercase = 'A';
+int isupper = 0;
+
+for (; uppercase <= 'Z'; uppercase++)
+{
+if (c == uppercase)
+{
+isupper = 1;
+break;
 }
+}
+return (isupper);
+}
+
+
 
 
