16799652e7a3ddc64cb512085766a27e384292d1
Sn3pcity <sn3pcity@gmail.com>
2022-10-25 18:47:01 -0700

    Rev Psychology

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,12 +6,12 @@
  */
 void print_rev(char *s)
 {
-int sn = 0, index;
+int len = 0, index;
 
 while (s[index++])
-sn++;
+len++;
 
-for (index = sn - 1; index >= 0; index--)
+for (index = len - 1; index >= 0; index--)
 _putchar(s[index]);
 
 _putchar('\n');
