5020fe9e454b937c5f899a98822bdf02a2cb9f04
Sn3pcity <sn3pcity@gmail.com>
2022-11-16 17:43:57 -0800

    Task 0

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,10 @@
+C - Structures, typedef
+Define a new type struct dog with the following elements:
+
+
+
+name, type = char *
+
+age, type = float
+
+owner, type = char *
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure for a basic dog
+ * @name: name of dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Description: data structure for dog
+ */
+struct dog
+
+{
+char *name;
+float age;
+char *owner;
+};
