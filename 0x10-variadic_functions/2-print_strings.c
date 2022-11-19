f75cd6eeb897560750f2a5f6b8bd8dccb2b26fe9
Snap <113688601+Sn3pcity@users.noreply.github.com>
2022-11-20 01:55:19 +0300

    Update 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,6 +1,6 @@
+#include <stdio.h>
 #include "variadic_functions.h"
 #include <stdarg.h>
-#include <stdio.h>
 
 /**
  * print_strings - checks numbers to be printed
@@ -14,12 +14,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 unsigned int s;
 char *str;
-va_list sa;
+va_list sa;/** *variable(sa) to access the unnamed arguments**/
 
-va_start(sa, n);
+va_start(sa, n);/** *The first is the variable declared of the type va_list & 2nd is name of the last named parameter of the function in this it being n**/
 for (s = 0; s < n; s++)
 {
-str = va_arg(sa, char*);
+str = va_arg(sa, char*);/** *The first argument to va_arg is the va_list and the second is the type of the next argument passed to the function.**/
 if (str == NULL)
 printf("(nil)");
 else
@@ -32,5 +32,5 @@ printf("%s", separator);
 }
 
 printf("\n");
-va_end(sa);
+va_end(sa);/** *must be called on the va_list before the function returns. (It is not required to read in all the arguments.)**/
 }
