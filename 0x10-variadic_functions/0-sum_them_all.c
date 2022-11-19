409b9bd1075afc0d768bf5abdb2630006468f1c4
Sn3pcity <sn3pcity@gmail.com>
2022-11-19 06:45:01 -0800

    Task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - function that sums everything
+ *@n: number to be added.
+ * Return: Always 0.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+if (n == 0)
+return (0);
+else
+return (1);
+}
