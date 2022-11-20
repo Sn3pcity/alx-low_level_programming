a1b50f1842f96055b93d1a36a36355a9a52792fc
Snap <113688601+Sn3pcity@users.noreply.github.com>
2022-11-20 04:01:56 +0300

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,11 +15,14 @@ va_list sa; /**
 unsigned int s;
 int sum = 0;
 
-if (n == 0)
-return (0);
-va_start(sa, n);
+va_start(sa, n);/** 
+*1st argument is va_list & 2nd is no. of arguments to be passed on func
+**/
 for (s = 0; s < n; s++)
 sum += va_arg(sa, int);
+  
+if (n == 0)
+return (0);
 
 va_end(sa);
 return (sum);
