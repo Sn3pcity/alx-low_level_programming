b1e863931435c9afe2a08867991a5786009b1621
Sn3pcity <sn3pcity@gmail.com>
2023-06-29 04:39:08 -0700

    update

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,5 +11,20 @@ if (n == 1)
 return (0);
 }
 else
-return (is_prime_number(n, n / 2));
+return (_prime(n, n / 2));
+}
+/**
+ *_prime - returns un if n is prime
+ *@n: number to be checked
+ *@s: number
+"*Return: number
+ */
+
+int _prime(int n, int s)
+{
+if (s == 1)
+return (1);
+if (n % s == 0)
+return (0);
+return (_prime(n, s - 1));
 }
